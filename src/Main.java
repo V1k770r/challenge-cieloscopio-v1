f2ab28e5bc0ba4bfbd369be00550b28392f90891
V1k770r <testprueba066@gmail.com>
2024-07-18 00:08:05 -0500

    version 1.0

---
+++ src/Main.java
@@ -0,0 +1,115 @@
+import com.google.gson.*;
+import modelos.InformacionClima;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Main {
+
+    private static final String API_KEY = "01c9a3da1cb2d196a7eced3b1fd3a7f7";
+    private static final String BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner lectura = new Scanner(System.in);
+        var respuesta = 0;
+        while (respuesta != 7) {
+            System.out.println(
+                    """
+
+                             Challenge Cieloscopio:\s
+                            ------------------------------------
+                            Elige una ciudad para obtener los datos meteorológicos:\s
+                            1. Ciudad de México \s
+                            2. Buenos Aires\s
+                            3. Bogotá\s
+                            4. Lima\s
+                            5. Santiago\s
+                            6. Deseo consultar otra ciudad\s
+                            7. Salir\s
+                            ------------------------------------"""
+            );
+
+            System.out.println("Elige una opcion basada en el numero: ");
+            respuesta = Integer.parseInt(lectura.nextLine());
+
+            switch (respuesta) {
+                case 1 -> busquedaDeDatosPorCiudad("Ciudad de Mexico");
+                case 2 -> busquedaDeDatosPorCiudad("Buenos Aires");
+                case 3 -> busquedaDeDatosPorCiudad("Bogota");
+                case 4 -> busquedaDeDatosPorCiudad("Lima");
+                case 5 -> busquedaDeDatosPorCiudad("Santiago");
+                case 6 -> {
+                    System.out.println("Escriba el nombre de una ciudad: ");
+                    var busquedaCiudad = lectura.nextLine();
+                    busquedaDeDatosPorCiudad(busquedaCiudad);
+                }
+                case 7 -> {
+                    System.out.println("Saliendo del programa Cieloscopio, hasta luego....");
+                    break;
+                }
+                default -> {
+                    System.out.println("Opcion no valida, intentelo de nuevo.\n");
+                }
+            }
+        }
+    }
+
+    private static void busquedaDeDatosPorCiudad(String ciudad) throws IOException, InterruptedException {
+
+        ciudad = ciudad.replace(" ", "+");
+        String direccion = BASE_URL + String.format("?q=%s&appid=%s&units=metric&lang=es", ciudad, API_KEY);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(direccion))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create();
+
+        JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
+
+        String nombreCiudad = jsonObject.get("name").getAsString();
+        double temperaturaActual = jsonObject.getAsJsonObject("main").get("temp").getAsDouble();
+        double temperaturaMinima = jsonObject.getAsJsonObject("main").get("temp_min").getAsDouble();
+        double temperaturaMaxima = jsonObject.getAsJsonObject("main").get("temp_max").getAsDouble();
+        String condicionClimatica = jsonObject.getAsJsonArray("weather").get(0).getAsJsonObject().get("description")
+                .getAsString();
+
+        InformacionClima respuestaInformacion = new InformacionClima(nombreCiudad, temperaturaActual, temperaturaMinima,
+                temperaturaMaxima, condicionClimatica);
+        presentarRespuesta(respuestaInformacion);
+    }
+
+    private static void presentarRespuesta(InformacionClima respuesta) {
+        DateTimeFormatter formatterDay = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter formatterHour = DateTimeFormatter.ofPattern("HH:mm");
+
+        String respuestaEnTexto = "-----------------------------\n" +
+                "Respuesta: \n" +
+                "Ciudad: " + respuesta.getNombre() + "\n" +
+                "Fecha: " + respuesta.getFechaDeSolicitud().format(formatterDay) + "\n" +
+                "Horario: " + respuesta.getFechaDeSolicitud().format(formatterHour) + "\n" +
+                "\n" +
+                "Temperatura actual: " + respuesta.getTemperaturaActual() + "\n" +
+                "Condicion climatica: " + respuesta.getCondicionClimatica() + "\n" +
+                "Temperatura minima: " + respuesta.getTemperaturaMinima() + "\n" +
+                "Temperatura maxima: " + respuesta.getTemperaturaMaxima() + "\n" +
+                "------------------";
+        System.out.println(respuestaEnTexto);
+
+    }
+}
---
+++ src/modelos/InformacionClima.java
@@ -0,0 +1,91 @@
+package modelos;
+
+import com.google.gson.annotations.SerializedName;
+import java.time.LocalDateTime;
+
+public class InformacionClima {
+    @SerializedName("name")
+    private String nombre;
+
+    private LocalDateTime fechaDeSolicitud;
+    @SerializedName("temp")
+    private double temperaturaActual;
+    @SerializedName("temp_min")
+    private double temperaturaMinima;
+    @SerializedName("temp_max")
+    private double temperaturaMaxima;
+    @SerializedName("description")
+    private String condicionClimatica;
+
+    public InformacionClima(){
+    }
+
+    public InformacionClima(String nombre, double temperaturaActual, double temperaturaMinima, double temperaturaMaxima, String condicionClimatica) {
+        this.nombre = nombre;
+        this.fechaDeSolicitud = LocalDateTime.now();
+        this.temperaturaActual = temperaturaActual;
+        this.temperaturaMinima = temperaturaMinima;
+        this.temperaturaMaxima = temperaturaMaxima;
+        this.condicionClimatica = condicionClimatica;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDateTime getFechaDeSolicitud() {
+        return fechaDeSolicitud;
+    }
+
+    public void setFechaDeSolicitud(LocalDateTime fechaDeSolicitud) {
+        this.fechaDeSolicitud = fechaDeSolicitud;
+    }
+
+    public double getTemperaturaActual() {
+        return temperaturaActual;
+    }
+
+    public void setTemperaturaActual(double temperaturaActual) {
+        this.temperaturaActual = temperaturaActual;
+    }
+
+    public double getTemperaturaMinima() {
+        return temperaturaMinima;
+    }
+
+    public void setTemperaturaMinima(double temperaturaMinima) {
+        this.temperaturaMinima = temperaturaMinima;
+    }
+
+    public double getTemperaturaMaxima() {
+        return temperaturaMaxima;
+    }
+
+    public void setTemperaturaMaxima(double temperaturaMaxima) {
+        this.temperaturaMaxima = temperaturaMaxima;
+    }
+
+    public String getCondicionClimatica() {
+        return condicionClimatica;
+    }
+
+    public void setCondicionClimatica(String condicionClimatica) {
+        this.condicionClimatica = condicionClimatica;
+    }
+
+//    @Override
+//    public String toString() {
+//        return "InformacionClima{" +
+//                "nombre='" + nombre + '\'' +
+//                ", fechaDeSolicitud=" + fechaDeSolicitud +
+//                ", temperaturaActual=" + temperaturaActual +
+//                ", temperaturaMinima=" + temperaturaMinima +
+//                ", temperaturaMaxima=" + temperaturaMaxima +
+//                ", condicionClimatica='" + condicionClimatica + '\'' +
+//                '}';
+//    }
+}
